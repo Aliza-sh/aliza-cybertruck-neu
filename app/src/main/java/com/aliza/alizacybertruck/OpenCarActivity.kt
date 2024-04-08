02e6d959117e3e5d107f98fee1250ee6677f8c70
Aliza <alizashahsavari@gmail.com>
2024-04-09 00:54:08 +0330

    Page design OpenCarActivity

--- app/src/main/java/com/aliza/alizacybertruck/OpenCarActivity.kt
+++ app/src/main/java/com/aliza/alizacybertruck/OpenCarActivity.kt
@@ -2,10 +2,10 @@ package com.aliza.alizacybertruck
 
 import android.os.Bundle
 import com.aliza.alizacybertruck.base.BaseActivity
-import com.aliza.alizacybertruck.databinding.ActivityMainBinding
+import com.aliza.alizacybertruck.databinding.ActivityOpenCarBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-    override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
+class OpenCarActivity : BaseActivity<ActivityOpenCarBinding>() {
+    override fun inflateBinding() = ActivityOpenCarBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
